4cbd1a98d89e5a086d1a5add6093733ad2413ad8
JorgeAndd <jorgeluizandrade@outlook.com>
2020-06-11 11:30:11 -0300

    Position change on map click

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, ChangeEvent } from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
 import axios from 'axios';
 import api from '../../services/api';
@@ -25,6 +26,11 @@ interface City {
   name: string;
 }
 
+interface MapPosition {
+  latitude: number;
+  longitude: number;
+}
+
 interface IBGEUFResponse {
   id: number;
   nome: string;
@@ -39,8 +45,13 @@ const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
   const [ufs, setUfs] = useState<UF[]>([]);
   const [cities, setCities] = useState<City[]>([]);
+
   const [selectedUf, setSelectedUf] = useState<string>();
   const [selectedCity, setSelectedCity] = useState<string>();
+  const [selectedPosition, setSelectedPosition] = useState<MapPosition>({
+    latitude: 0,
+    longitude: 0,
+  });
 
   useEffect(() => {
     api.get('items').then(response => {
@@ -100,6 +111,13 @@ const CreatePoint = () => {
     setSelectedCity(event.target.value);
   }
 
+  function handleMapClick(event: LeafletMouseEvent) {
+    setSelectedPosition({
+      latitude: event.latlng.lat,
+      longitude: event.latlng.lng,
+    });
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -137,13 +155,19 @@ const CreatePoint = () => {
           </div>
         </fieldset>
 
-        <Map center={[-15.7540088, -47.9009033]} zoom={15}>
+        <Map
+          center={[-15.7540088, -47.9009033]}
+          zoom={15}
+          onClick={handleMapClick}
+        >
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          <Marker position={[-15.7540088, -47.9009033]} />
+          <Marker
+            position={[selectedPosition.latitude, selectedPosition.longitude]}
+          />
         </Map>
 
         <fieldset>
