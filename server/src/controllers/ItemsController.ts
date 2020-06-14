bf74e3cb4f0fdc364018f28d2c2ecd6c9b55f300
JorgeAndd <jorgeluizandrade@outlook.com>
2020-06-14 11:03:26 -0300

    Fix local address on server for item image url

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from 'express';
-import knex from '../database/connection';
+import { Request, Response } from "express";
+import knex from "../database/connection";
 
 class ItemsController {
   async index(request: Request, response: Response) {
-    const items = await knex('Item').select('*');
-    const serializedItems = items.map(i => {
+    const items = await knex("Item").select("*");
+    const serializedItems = items.map((i) => {
       return {
         id: i.id,
         title: i.title,
-        image_url: `http://localhost:3333/assets/${i.image}`,
-      }
-    })
-  
+        image_url: `http://192.168.0.17:3333/assets/${i.image}`,
+      };
+    });
+
     return response.json(serializedItems);
   }
 }
